10126bbb078667adf4c0d6ad967ae78fbdcf0209
solvesweb <solvesweb@gmail.com>
2023-11-23 18:12:18 +0100

    Primer Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { AddCategory } from "./Components/AddCategory";
+import { GifGrid } from "./Components/GifGrid";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GIF APP - Tu buscador de Gifs</h1>
+
+      <AddCategory
+        // setCategories={setCategories}
+        onNewCategory={(value) => onAddCategory(value)}
+      />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
+
+export default GifExpertApp;
